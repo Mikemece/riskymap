6cf8095da0c2bd41d2d94558741fbc32c71333e8
Mikemece <mcmiguel313@gmail.com>
2024-05-01 00:36:36 +0200

    🛠️ Ocultados datos de la BD

--- backend/firebaseConfig.ts
+++ backend/firebaseConfig.ts
@@ -7,12 +7,12 @@ import { getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSHDjHnx_ShxPh_EP7zQV40VE6sdLgIJk",
-  authDomain: "riskymap-93a25.firebaseapp.com",
-  projectId: "riskymap-93a25",
-  storageBucket: "riskymap-93a25.appspot.com",
-  messagingSenderId: "579378733038",
-  appId: "1:579378733038:web:1d220d6a0e2eea76aa38ff"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
